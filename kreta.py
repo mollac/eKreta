14852eca6d4c741b75e98a1f8c0f479952b48896
L. Molnár <molnar.laci@outlook.com>
2020-03-04 09:12:03 +0100

    ha van e-üzenet, az is megjelenik + formázások

--- kreta.py
+++ kreta.py
@@ -36,37 +36,46 @@ for k, v in urls.items():
     wd.find_element_by_id('UserName').send_keys(v[1])
     wd.find_element_by_id('Password').send_keys(v[2])
     wd.find_element_by_id('btnSubmit').click()
-    sleep(3)
+    sleep(2)
 
     tabla = wd.find_element_by_id('legutobbiErtekelesek')
     jegyek = tabla.find_elements_by_class_name('k-link')
 
     tabla = wd.find_element_by_id('legutobbiMulasztasok')
     mulasztasok = tabla.find_elements_by_class_name('k-link')
-    
-    email_data += f'<hr/><h2>JEGYEK/{k}</h2>'
+    email_data += f'<h2>{k}</h2>'
+    email_data += f'<h3>JEGYEK</h3>'
     for jegy in jegyek:
         t = jegy.text.split('\n')
-        email_data += f'<p>{t[2]:<14} <strong>{t[0]}</strong> {t[1]:<10}</p>'
+        email_data += f'<code>{t[0]} - {t[1]}\t - {t[2]}</code><br/>'
 
-    email_data += f'<hr/><h2>HIÁNYZÁSOK/{k}</h2>'
+    email_data += f'<h3>HIÁNYZÁSOK</h3>'
     for mulasztas in mulasztasok:
         m = mulasztas.text.split('\n')
-        email_data += f'<p>{m[0]:<14} {m[3]:<10} {m[4]}</p>'
-email_data += '<hr/'            
+        email_data += f'<code>{m[0]}, {m[3]} -> {m[4]}</code><br/>'
+    tabla = wd.find_element_by_id('legutobbiFeljegyzesek')
+    uzenet = tabla.find_elements_by_class_name('k-link')
+    if not 'Nincsenek' in uzenet[0].text:
+        tabla.click()
+        sleep(2)
+        uzenet = wd.find_element_by_xpath('//*[@id="InformaciokFeljegyzesekGrid"]/div[2]/div[1]/table/tbody/tr/td[6]/span')
+        email_data += f'<h3>ELEKTRONIKUS ÜZENET</h3>'
+        email_data += f'<code>{uzenet.text}</code><br/>'
+        
 wd.close()
+wd.quit()
 
 print('Email küldés.')
 
 email = EmailMessage()
 email['from'] = email_user
-email['to'] = 'cimzett1@email.com'
-email['cc'] = 'cimzett2@email.com'
+email['to'] = 'l.molnar@outlook.hu'
+email['cc'] = 'bona.eniko@outlook.com'
 email['subject'] = 'Kréta jegyek'
 
 email.set_content(html.substitute(data=email_data), 'html')
 
-with smtplib.SMTP(host='smtp szerver címe => port szám', port=587) as s:
+with smtplib.SMTP(host='SMTP.office365.com', port=587) as s:
     s.ehlo()
     s.starttls()
     s.login(email_user, email_password)
